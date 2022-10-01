06072b1bc45166e16316e9c26ceaaccf02148311
Miguel <miguelj0916@gmail.com>
2022-10-01 10:26:47 -0500

    repo variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+variable "app_repository" {
+  default = "https://github.com/iukion/CopaHackathon_html_base"
+  description = "Copa's application for deployment"
+}
 variable "resource_group_name" {
   default = "myTFResourceGroup"
 }
