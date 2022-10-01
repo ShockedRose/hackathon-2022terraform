498a3461083044750ed5e865eebbcf00100ba691
Miguel <miguelj0916@gmail.com>
2022-10-01 10:54:45 -0500

    ignore initial RG

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "app_repository" {
-  default = "https://github.com/iukion/CopaHackathon_html_base"
+  default     = "https://github.com/iukion/CopaHackathon_html_base"
   description = "Copa's application for deployment"
 }
 variable "resource_group_name" {
-  default = "myTFResourceGroup"
+  default = "terraform-challenge"
 }
 variable "app_region" {
   default = "westus2"
